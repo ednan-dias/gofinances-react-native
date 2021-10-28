cddc69e50e3e0a20cd3fc484a55ba49dba9e9ba3
Ednan Dias <ednan.741@gmail.com>
2021-10-28 16:57:32 -0300

    adicinando modal de categorias

--- App.tsx
+++ App.tsx
@@ -13,6 +13,8 @@ import {
 import theme from "./src/global/styles/theme";
 
 import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
+import { CategorySelect } from "./src/screens/CategorySelect";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +30,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" />
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { categories } from "../../utils/categories";
+import { FlatList } from "react-native";
+
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer,
+} from "./styles";
+import { Button } from "../../components/Form/Button";
+
+interface Category {
+  key: string;
+  name: string;
+}
+
+interface Props {
+  category: Category;
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
+}
+
+export function CategorySelect({
+  category,
+  setCategory,
+  closeSelectCategory,
+}: Props) {
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
+
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: "100%" }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category
+            onPress={() => setCategory(item)}
+            isActive={category.key === item.key}
+          >
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+
+      <Footer>
+        <Button title="Selecionar" onPress={closeSelectCategory} />
+      </Footer>
+    </Container>
+  );
+}
